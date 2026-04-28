90d548dac46a477edb50fe0a8119f364efc3e586
Chu A. <chu.amk22@gmail.com>
2026-04-28 09:51:07 +0100

    feat: refactor authentication controllers to implement session creation and user retrieval

--- server/src/modules/auth/auth.controller.js
+++ server/src/modules/auth/auth.controller.js
@@ -1,7 +1,25 @@
 import { sendSuccess } from '../../shared/http.js'
-import { getAuthStatus } from './auth.service.js'
+import { createSession, getProfile, logout } from './auth.service.js'
 
-export function getAuthStatusController(req, res) {
-  const data = getAuthStatus()
+export async function createSessionController(req, res, next) {
+  try {
+    const user = await createSession(req.user)
+    sendSuccess(req, res, { user })
+  } catch (error) {
+    next(error)
+  }
+}
+
+export async function getMeController(req, res, next) {
+  try {
+    const user = await getProfile(req.user)
+    sendSuccess(req, res, { user })
+  } catch (error) {
+    next(error)
+  }
+}
+
+export function logoutController(req, res) {
+  const data = logout()
   sendSuccess(req, res, data)
 }
