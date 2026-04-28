e64629ef8be0cdb3c0ef81a7284b2bc84092ed0f
Chu A. <chu.amk22@gmail.com>
2026-04-28 10:20:50 +0100

    feat: implement budget management controllers for listing, upserting, and deleting budgets

--- server/src/modules/budgets/budgets.controller.js
+++ server/src/modules/budgets/budgets.controller.js
@@ -1,7 +1,29 @@
 import { sendSuccess } from '../../shared/http.js'
-import { getBudgetsStatus } from './budgets.service.js'
+import { deleteUserBudget, listBudgets, upsertUserBudget } from './budgets.service.js'
 
-export function getBudgetsStatusController(req, res) {
-  const data = getBudgetsStatus()
-  sendSuccess(req, res, data)
+export async function listBudgetsController(req, res, next) {
+  try {
+    const data = await listBudgets(req.user, req.validated.query)
+    sendSuccess(req, res, data)
+  } catch (error) {
+    next(error)
+  }
+}
+
+export async function upsertBudgetController(req, res, next) {
+  try {
+    const budget = await upsertUserBudget(req.user, req.validated.body)
+    sendSuccess(req, res, { budget })
+  } catch (error) {
+    next(error)
+  }
+}
+
+export async function deleteBudgetController(req, res, next) {
+  try {
+    await deleteUserBudget(req.user, req.validated.params.budgetId)
+    sendSuccess(req, res, { deleted: true })
+  } catch (error) {
+    next(error)
+  }
 }
