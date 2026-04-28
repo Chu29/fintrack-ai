4289165d6a73cc1a70f823c1abd092a75bee861f
Chu A. <chu.amk22@gmail.com>
2026-04-28 09:00:55 +0100

    fix: update CORS configuration to use env variable for CLIENT_URL

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import helmet from 'helmet'
 import cookieParser from 'cookie-parser'
 import PinoHttp from 'pino-http'
+import { env } from './src/config/env.js'
 import { requestIdMiddleware } from './src/middleware/requestId.js'
 import { notFoundHandler, errorHandler } from './src/middleware/errors.js'
 // import { apiRouter } from './src/routes/api.js'
@@ -10,7 +11,7 @@ import { notFoundHandler, errorHandler } from './src/middleware/errors.js'
 export const app = express()
 
 app.use(helmet())
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }))
+app.use(cors({ origin: env.CLIENT_URL, credentials: true }))
 app.use(express.json())
 app.use(cookieParser())
 app.use(PinoHttp())
