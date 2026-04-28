b8fbbd6843041eb156aa9bf7541804be6fef06b7
Chu A. <chu.amk22@gmail.com>
2026-04-28 09:51:21 +0100

    feat: implement user and Firebase account management functions in auth repository

--- server/src/modules/auth/auth.repository.js
+++ server/src/modules/auth/auth.repository.js
@@ -1,2 +1,37 @@
-export const authRepository = {}
+import { prisma } from '../../config/prisma.js'
 
+export function findUserByEmail(email) {
+  return prisma.user.findUnique({
+    where: { email },
+  })
+}
+
+export function createUserByEmail(email) {
+  return prisma.user.create({
+    data: { email },
+  })
+}
+
+export function findFirebaseAccountWithUser(uid) {
+  return prisma.oAuthAccount.findUnique({
+    where: {
+      provider_providerAccountId: {
+        provider: 'firebase',
+        providerAccountId: uid,
+      },
+    },
+    include: {
+      user: true,
+    },
+  })
+}
+
+export function createFirebaseAccount({ userId, uid }) {
+  return prisma.oAuthAccount.create({
+    data: {
+      userId,
+      provider: 'firebase',
+      providerAccountId: uid,
+    },
+  })
+}
